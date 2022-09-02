182e61314fcb2faf147d550bc2ab299ac0d649f6
mdjakirkhan4928@gmail.com <mdjakirkhan4928@gmail.com>
2022-09-02 14:38:49 -0700

    favicon added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const loadNews = async()=>{
-    const url = `https://openapi.programming-hero.com/api/news/category/04`
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
     const res = await fetch(url);
     const data = await res.json();
     displayNews(data.data)
